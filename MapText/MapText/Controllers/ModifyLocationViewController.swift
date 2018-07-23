d9ad9adacd014e4f711f81f7afe1bf920721ff7f
Ronald Chan <ronald.chan850@gmail.com>
2018-07-23 15:54:17 -0700

    connected storyboard UI items with respective classes

---
+++ MapText/MapText/Controllers/ModifyLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ModifyLocationViewController.swift
+//  MapText
+//
+//  Created by Ronald Chan on 7/23/18.
+//  Copyright © 2018 Ronald Chan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ModifyLocationViewController:UIViewController {
+    
+}
---
+++ MapText/MapText/Views/DisplayLocationTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DisplayLocationTableViewCell.swift
+//  MapText
+//
+//  Created by Ronald Chan on 7/23/18.
+//  Copyright © 2018 Ronald Chan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class DisplayLocationTableViewCell:UITableViewCell {
+    
+    @IBOutlet weak var locationName: UILabel!
+    @IBOutlet weak var locationCoordinates: UILabel!
+    @IBOutlet weak var locationActive: UISwitch!
+}
